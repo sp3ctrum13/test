0125dccf6dbb8e20bc0f8f96e91d6020cfee54ca
sp3ctrum13 <sp3-@hotmail.com>
2021-12-25 20:41:43 +0100

    Create TP 3.cpp
    Res TP3 c++

---
+++ TP 3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int puissance(int a, int b){
+	
+
+	return pow(a,b);
+}
+int main(int argc, char** argv) {
+	int a,b;
+	cout << "Entrer a et b";
+	cin >> a;
+	cin >> b;
+	cout << "a exposant b = " << puissance(a,b) << "\n"  ;
+	
+	return 0;
+}
+
+
+
